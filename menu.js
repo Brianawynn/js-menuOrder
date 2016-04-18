0edd24da285d76056898ce06e08a9b9faf497a72
Brianawynn <wynn.briana@wustl.edu>
2016-04-18 18:41:08 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -8,21 +8,22 @@ var mainFood = prompt("Would you like a taco, burrito, or a bowl");
 function question(questionText) {
   var ansInput = prompt(questionText);
   // your code goes here
+   order = order + ansInput;
 } 
 
 
 order = mainFood+ " with ";
 // call the QUESTION function with the order options
 question("What type of meat would you like?");
- order = order + ansInput;
+ 
 question("Would you like lettuce?");
- order = order + ansInput;
+ 
 question("Would you like tomato?");
- order = order + ansInput;
+ 
 question("Would you like rice?");
- order = order + ansInput;
+ 
 question("Would you like salsa");
- order = order + ansInput;
+ 
 
 // output their order
 document.write("<p>You ordered: " + order + "<p>");
