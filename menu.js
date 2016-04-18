c4c4d873f9e22d181296e32b51482f0264e9f764
Brianawynn <wynn.briana@wustl.edu>
2016-04-18 18:37:31 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -14,11 +14,14 @@ function question(questionText,) {
 order = mainFood+ " with ";
 // call the QUESTION function with the order options
 question("What type of meat would you like?");
+ order = order + ansInput;
 question("Would you like lettuce?");
+ order = order + ansInput;
 question("Would you like tomato?");
+ order = order + ansInput;
 question("Would you like rice?");
+ order = order + ansInput;
 question("Would you like salsa");
-
  order = order + ansInput;
 
 // output their order
