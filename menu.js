e75e5c1cafec0da27bb65ce0681e49691fe2fbc9
Brianawynn <wynn.briana@wustl.edu>
2016-04-18 18:34:57 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -3,13 +3,23 @@ var order = '';
 
 
 // build a function to ask questions
+var mainFood = prompt("Would you like a taco, burrito, or a bowl");
 
-function question() {
+function question(questionText,) {
+  var ansInput = prompt(questionText)
   // your code goes here
-}
+} 
 
+
+order = mainFood+ " with ";
 // call the QUESTION function with the order options
-question();
+question("What type of meat would you like?");
+question("Would you like lettuce?");
+question("Would you like tomato?");
+question("Would you like rice?");
+question("Would you like salsa");
+
+ order = order + ansInput;
 
 // output their order
 document.write("<p>You ordered: " + order + "<p>");
