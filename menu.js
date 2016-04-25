b531856e842fa137a1210d40cfe41dd6b040bcf7
Brianawynn <wynn.briana@wustl.edu>
2016-04-25 18:18:53 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -9,22 +9,30 @@ var l = 'lettuce';
 var t = 'tomato';
 var r = 'rice';
 var s = 'salsa';
+var [] question = ["What type of meat would you like?", "Would you like lettuce or spinach?", "Would you like brown or white rice", Would you like green or red salsa?];
 
 function question(questionText) {
   var ansInput = prompt(questionText);
   // your code goes here
     order = order + ""+ ansInput + "";
 } 
-
+// for ( int i = 0; i< 4; i++){
+//   question [i] = ansInput;
+// }
+// while ( i < question.length){
+//   order = mainFood + " with " + question[i];
+  
+// }
 order = mainFood+ " with ";
 // call the QUESTION function with the order options
-question("What type of meat would you like?");
+question(question);
+// question("What type of meat would you like?");
+
+// question("Would you like lettuce or spinach?");
  
-question("Would you like lettuce or spinach?");
+// question("Would you like brown rice or white rice?");
  
-question("Would you like brown rice or white rice?");
- 
-question("Would you like green salsa or red salsa");
+// question("Would you like green salsa or red salsa");
  
 
 // output their order
